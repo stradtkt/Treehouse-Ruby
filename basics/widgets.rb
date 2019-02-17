bd3f7d65f242076c4e5473e302163cf5bc946309
Kevin <stradtkt22@gmail.com>
2019-02-17 17:28:13 -0500

    Finished the widget.rb file

--- basics/widgets.rb
+++ basics/widgets.rb
@@ -5,6 +5,19 @@ def ask(question)
   gets
 end
 
+def price(quantity)
+  if quantity >= 100
+    price_per_unit = 8
+  end
+  if quantity >= 50 && quantity < 100
+    price_per_unit = 9
+  end
+  if quantity < 50
+    price_per_unit = 10
+  end
+  quantity * price_per_unit
+end
+
 answer = ask("How many widgets are you going to order?  ")
 number = answer.to_i
 line = "You entered #{number} widgets, your total is: $#{number * 10}"
