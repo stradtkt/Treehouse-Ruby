ad2c4c2668f5a2b52cf5805643549fbce916d771
Kevin <stradtkt22@gmail.com>
2019-02-18 22:05:22 -0500

    Added blocks

---
+++ blocks/blocks_new.rb
@@ -0,0 +1,13 @@
+loop do
+  puts "Hello world"
+end
+
+loop {
+  puts "Hello world"
+}
+
+3.times do
+  puts "Hello world"
+end
+
+3.times {puts "Hello world"}
---
+++ collections/hashes.rb
@@ -0,0 +1,6 @@
+item = {"name" => "Bread"}
+puts item
+
+hash = {"item" => "Bread", "brand" => "Kroger Brand"}
+hash.has_value?(1)
+hash.value?(1)
