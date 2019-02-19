fb63cbf0e45374f0892c8b8f37ce3390b21e1be4
Kevin <stradtkt22@gmail.com>
2019-02-18 22:26:29 -0500

    Added more to blocks

--- blocks/blocks_new.rb
+++ blocks/blocks_new.rb
@@ -11,3 +11,24 @@ loop {
 end
 
 3.times {puts "Hello world"}
+
+def block_method
+  puts "This is the first line in block_method"
+  yield
+end
+
+block_method do
+  puts "This statement is called from the block"
+end
+
+[1,2,3].each {|number| puts "Number: #{number}"}
+
+def get_name
+  print "Enter your name: "
+  name = gets.chomp
+  yield name
+end
+
+get_name do |name|
+  print "That's a cool name, #{name}!"
+end
