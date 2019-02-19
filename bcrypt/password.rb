a06a82244ccff10bd8543a458f0e6e763299d128
Kevin <stradtkt22@gmail.com>
2019-02-18 23:47:13 -0500

    Added users

--- bcrypt/password.rb
+++ bcrypt/password.rb
@@ -1,5 +1,12 @@
 require 'bcrypt'
 
+users = [
+    {username: "mike1", password: "abc123"},
+    {username: "kelly1", password: "password1"},
+    {username: "patrick1", password: "password2"},
+    {username: "kevin1", password: "password3"}
+]
+
 def create_hash_digest(password)
   BCrypt::Password.create(password)
 end
